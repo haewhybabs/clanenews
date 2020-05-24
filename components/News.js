4b350b454c1440f844affdc2e5844cc3bed84b38
Haewhybabs <babalolaisaac@gmail.com>
2020-05-24 14:59:22 +0100

    News Page API integration

--- components/News.js
+++ components/News.js
@@ -25,6 +25,8 @@ class News extends Component{
             isLoading:true,
             page:1,
             limit:10,
+            length:null,
+            maxPage:null,
             dataSource:[],
            
             
@@ -33,16 +35,24 @@ class News extends Component{
        
     }
 
+
+
    
     componentDidMount() {
+        
+        this.fetchData();  
+        this.fetchLength()       
+    }
 
-        fetch(apiUrl+'news?page='+this.state.page+'&limit='+this.state.limit,{
+    fetchLength = async() =>
+    {
+        fetch(apiUrl+'news',{
             method:"GET",
             headers: {
+
                 'Content-Type': 'application/json',
                 'Accept':'application/json'
-            }
-            
+            }     
         })
         .then(response => {
                                 
@@ -51,8 +61,39 @@ class News extends Component{
         
         .then((contents)=>{
 
-            this.setState({
+            const length =contents.length;
+            let maxPage =1;
+            if(length){
+                maxPage =Math.ceil(length/this.state.limit);
+            }
+            
+            
+            this.setState({length,maxPage});
+        })
+        .catch((error)=>{
 
+            this.errorInConnection();
+        })
+    }
+
+    fetchData = () => 
+    {
+        console.log(this.state.page)
+        fetch(apiUrl+'news?page='+this.state.page+'&limit='+this.state.limit,{
+            method:"GET",
+            headers: {
+
+                'Content-Type': 'application/json',
+                'Accept':'application/json'
+            }     
+        })
+        .then(response => {
+                                
+            return response.json();      
+        })
+        
+        .then((contents)=>{
+            this.setState({
                 dataSource:contents,
                 isLoading:false
 
@@ -62,20 +103,15 @@ class News extends Component{
 
             this.errorInConnection();
         })
-       
-       
-        
     }
 
     errorInConnection = () => {
         this.hideLoader();
-
         Toast.show({
             text:'Ops!! Connection Problem',
             buttonText:'Okay',
-            style:{backgroundColor:'red'}
-            
-        })
+            style:{backgroundColor:'red'}     
+        });
     }
     showLoader = () => {
         this.setState({isLoading:true})
@@ -88,40 +124,67 @@ class News extends Component{
     ReadHandler = () =>{
         this.props.navigation.navigate('NewsDetails')
     }
+    nextHandler = () =>
+    {
+       
+        let newPage = this.state.page + 1;
+        if(newPage <=this.state.maxPage){
+            this.showLoader()
+            this.setState({
+                page:newPage
+            },()=>{
+                this.fetchData();
+            })
+            
+        }
+    }
+    backHandler = () =>
+    {
+        let newPage = this.state.page - 1;
+        if(newPage >0){
+            this.showLoader()
+            this.setState({
+                page:newPage
+            },()=>{
+                this.fetchData();
+            })
+            
+        }
+    }
 
     
 
   
     render(){
-        
-    
        
-            return (  
-                this.state.isLoading
-                ?
-                <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-                    <ActivityIndicator size="large" color="#00CCFF" animating  />
-                </View>
-                :
-                <Root>
-                    <Container style={{backgroundColor:'#fff'}}>
-                   
-                        <Content>
+        return (  
+            this.state.isLoading
+            ?
+            <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+                <ActivityIndicator size="large" color="#00CCFF" animating  />
+            </View>
+            :
+            <Root>
+                <Container style={{backgroundColor:'#fff'}}>
+                
+                    <Content>
+                        
+                        <Image source={require('../assets/news-background.jpg')}
+                            style={{width:'100%',height:300}}
+                        />
+                        
+                        <View style={{ alignItems:'center'}}>
+                            <Text style={{fontSize:15, marginTop:20, marginBottom:10}}>Latest News</Text>
+                        </View>
                             
-                            <Image source={require('../assets/news-background.jpg')}
-                                style={{width:'100%',height:300}}
-                            />
-                           
-                            <View style={{ alignItems:'center'}}>
-                                <Text style={{fontSize:15, marginTop:20, marginBottom:10}}>Latest News</Text>
-                            </View>
-                                
-                            <View style={{marginLeft:5,marginRight:5}}>
-                                <Card>
+                        <View style={{marginLeft:5,marginRight:5}}>
+
+                            {this.state.dataSource.map((row,index)=>(
+                                <Card key={row.id}>
                                     <CardItem>
                                         <Row>
                                             <Icon active name="logo-hackernews" />
-                                            <Text style={{fontWeight:'bold'}}>Corona Virus is up to 9000 cases in Nigeria</Text>
+                                            <Text style={{fontWeight:'bold'}}>{row.title}</Text>
                                         </Row>
                                         <Right>
                                             <Button rounded style={{width:60,height:30}} onPress={this.ReadHandler}>
@@ -130,73 +193,44 @@ class News extends Component{
                                         </Right>
                                     </CardItem>
                                     <CardItem>  
-                                        <Text>Ajani Adekemi</Text>
+                                        <Text>{row.author}</Text>
                                         <Right>
-                                            <Text>4-jan-2020</Text>
+                                            <Text>{row.createdAt}</Text>
                                         </Right>
                                     </CardItem>
                                 </Card>
-                                <Card>
-                                    <CardItem>
-                                        <Row>
-                                            <Icon active name="logo-hackernews" />
-                                            <Text style={{fontWeight:'bold'}}>Corona Virus is up to 9000 cases in Nigeria</Text>
-                                        </Row>
-                                        <Right>
-                                            <Button rounded style={{width:60,height:30}}>
-                                                <Text style={{textAlign:'center',width:'100%',color:'#fff'}}>Read</Text>
-                                            </Button>
-                                        </Right>
-                                    </CardItem>
-                                    <CardItem>  
-                                        <Text>Ajani Adekemi</Text>
-                                        <Right>
-                                            <Text>4-jan-2020</Text>
-                                        </Right>
-                                    </CardItem>
-                                </Card>
-
-                                <Card>
-                                    <CardItem>
-                                        <Row>
-                                            <Icon active name="logo-hackernews" />
-                                            <Text style={{fontWeight:'bold'}}>Corona Virus is up to 9000 cases in Nigeria</Text>
-                                        </Row>
-                                        <Right>
-                                            <Button rounded style={{width:60,height:30}}>
-                                                <Text style={{textAlign:'center',width:'100%',color:'#fff'}}>Read</Text>
-                                            </Button>
-                                        </Right>
-                                    </CardItem>
-                                    <CardItem>  
-                                        <Text>Ajani Adekemi</Text>
-                                        <Right>
-                                            <Text>4-jan-2020</Text>
-                                        </Right>
-                                    </CardItem>
-                                </Card>
-
-                                <View>
-                                    <Row>
-                                        
-                                        <Button transparent style={{width:60}}>
+                            ))}
+                            <View>
+                                <Row>
+                                    
+                                    {this.state.page>1 ?
+                                    
+                                        <Button transparent style={{width:60}} onPress={()=>{this.backHandler()}}>
                                             <Text style={{textAlign:'center',width:'100%',color:'red'}}>Previous</Text>
                                         </Button>
+                                        :null
+                                    }
                                     
+                                    {this.state.page ===this.state.maxPage ?
+                                        null
+                                        :
 
-                                        <Button transparent style={{width:60}}>
+                                        <Button transparent style={{width:60}} onPress={()=>{this.nextHandler()}}>
                                             <Text style={{textAlign:'center',width:'100%',color:'green'}}>Next</Text>
                                         </Button>
-                                    </Row>
-                                </View>
+                                    
+                                    }
+                                    
+                                </Row>
+                            </View>
 
-                                
-                            </View>       
-                        </Content>
-                        <FooterScreen navigation = {this.props.navigation}/>                    
-                    </Container>
-                </Root>
-            );
+                            
+                        </View>       
+                    </Content>
+                    <FooterScreen navigation = {this.props.navigation}/>                    
+                </Container>
+            </Root>
+        );
 
         
 
